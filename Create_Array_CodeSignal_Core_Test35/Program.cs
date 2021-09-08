5018c8c2947adf63847c1bb696d1b1520b7d7244
Albert Hakobyan <abhakobyan24@gmail.com>
2021-09-08 17:16:15 +0400

    Добавьте файлы проекта.

---
+++ Create_Array_CodeSignal_Core_Test35/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Create_Array_CodeSignal_Core_Test35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = createArray(5);
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static int[] createArray(int size)
+        {
+            List<int> list = new List<int>();
+            for (int i = 0; i < size; i++)
+            {
+                list.Add(1);
+            }
+            int[] array = list.ToArray();
+            return array;
+        }
+
+    }
+}
